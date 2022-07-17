2797419bc6be03f726b52e86a5a36f45a20880cc
Karthicashok <karthic.a@purpleslate.in>
2022-07-17 21:59:43 +0530

    changes in app.module

--- DataGovernance/src/app/app.module.ts
+++ DataGovernance/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './Component/login/login.component';
 import { HomeComponent } from './Component/home/home.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
@@ -45,7 +44,6 @@ const oktaAuth = new OktaAuth(environment.oktaconfig);
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
     DataTableComponent,
     LayersComponent,
